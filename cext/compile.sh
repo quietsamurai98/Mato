fc8e64eb69440b358b737c5b0985683c97385c98
Mike Martin <mike@unsupported.me>
2020-12-11 20:33:47 -0600

    working mac build, do not merge - needs cross platform changes

--- cext/compile.sh
+++ cext/compile.sh
@@ -11,6 +11,6 @@ fi
 
 cd ../../
 mkdir -p ./mygame/native/$PLATFORM
-COMPILER_FLAGS="-isystem ./include -I. -Ofast -flto=full -fopenmp"
+COMPILER_FLAGS="-isysroot $(xcrun --show-sdk-path) $(clang -E -xc++ -Wp,-v /dev/null 2>&1 | sed -n '/^#include <...>/,/^End of search/p'| sed '1d;$d;s/\/\(.*\)/-I \/\1/;s/ (framework directory)//') -isystem ./include -I. -I /usr/local/Cellar/libomp/11.0.0/include/ -L/usr/local/Cellar/libomp/11.0.0/lib/ -Ofast -flto -Xpreprocessor -fopenmp -lomp"
 ./dragonruby-bind --compiler-flags="$COMPILER_FLAGS" --ffi-module=MatoCore --output=./mygame/native/mato-bind.c ./mygame/cext/mato.c
-clang $COMPILER_FLAGS -fPIC -shared ./mygame/cext/src/*.c ./mygame/native/mato-bind.c -o ./mygame/native/$PLATFORM/matocore.$DLLEXT
+clang $COMPILER_FLAGS -v -fPIC -shared ./mygame/cext/src/*.c ./mygame/native/mato-bind.c -o ./mygame/native/$PLATFORM/matocore.$DLLEXT
